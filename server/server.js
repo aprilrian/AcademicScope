ce9660196506b4809d7dee148736cc326a43b175
ArdianFW <paskera87@gmail.com>
2023-11-06 10:14:25 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -52,39 +52,111 @@ app.post('/token', async (req, res) => {
   }
 })
 
-
 app.get('/users', authenticateToken, async (req, res) => {
-  const users = await db.query(`SELECT * FROM users`)
+  const users = await db.query(`SELECT * FROM public.users`)
   res.json(users.filter(user => user.username === req.user.username))
 })
 
+// Entry Pengambilan IRS per Semester (SRS-XXX-003)
+app.post('/irs', authenticateToken, async (req, res) => {
+  const { semester, courses } = req.body;
+  const username = req.user.username;
+
+  try {
+    // Simpan data IRS ke database
+    await db.query(`
+      INSERT INTO public.irs (username, semester, courses)
+      VALUES ($1, $2, $3)
+    `, [username, semester, courses]);
+
+    res.status(201).json({ message: 'Data IRS berhasil disimpan' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: 'Gagal menyimpan data IRS' });
+  }
+});
+
+// Entry Prestasi Akademik KHS per Semester (SRS-XXX-004)
+app.post('/khs', authenticateToken, async (req, res) => {
+  const { semester, grades } = req.body;
+  const username = req.user.username;
+
+  try {
+    // Simpan data KHS ke database
+    await db.query(`
+      INSERT INTO public.khs (semester_aktif, sks, sks_kumulatif, ip, ip_kumulatif, status_konfirmasi, file, mahasiswa_id)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, (SELECT id FROM public.mahasiswa WHERE nim = $8))
+    `, [semester, grades.sks, grades.sks_kumulatif, grades.ip, grades.ip_kumulatif, grades.status_konfirmasi, grades.file, username]);
+
+    res.status(201).json({ message: 'Data KHS berhasil disimpan' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: 'Gagal menyimpan data KHS' });
+  }
+});
+
+// Entry Progress PKL (SRS-XXX-005)
+app.post('/pkl', authenticateToken, async (req, res) => {
+  const { nilai, semester, status_konfirmasi, file } = req.body;
+  const username = req.user.username;
+
+  try {
+    // Simpan data progress PKL ke database
+    await db.query(`
+      INSERT INTO public.pkl (nilai, semester, status_konfirmasi, file, mahasiswa_id)
+      VALUES ($1, $2, $3, $4, (SELECT id FROM public.mahasiswa WHERE nim = $5))
+    `, [nilai, semester, status_konfirmasi, file, username]);
+
+    res.status(201).json({ message: 'Data progress PKL berhasil disimpan' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: 'Gagal menyimpan data progress PKL' });
+  }
+});
+
+// Entry Progress Skripsi (SRS-XXX-006) dengan Tanggal Lulus/Sidang dan Lama Studi dalam Semester
+app.post('/skripsi', authenticateToken, async (req, res) => {
+  const { nilai, tanggal, semester, status_konfirmasi, file } = req.body;
+  const username = req.user.username;
+
+  try {
+    // Simpan data progress skripsi ke database
+    await db.query(`
+      INSERT INTO public.skripsi (nilai, tanggal, semester, status_konfirmasi, file, mahasiswa_id)
+      VALUES ($1, $2, $3, $4, $5, (SELECT id FROM public.mahasiswa WHERE nim = $6))
+    `, [nilai, tanggal, semester, status_konfirmasi, file, username]);
+
+    res.status(201).json({ message: 'Data progress skripsi berhasil disimpan' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: 'Gagal menyimpan data progress skripsi' });
+  }
+});
+
 app.post('/register', async (req, res) => {
   const hashedPassword = await bcrypt.hash(req.body.password, 10)
-  const { username, email, password, role } = req.body
+  const { username, email, password, role } = req.body;
   try {
     const user = await db.query(`
-      INSERT INTO users (username, email, password, role)
+      INSERT INTO public.users (username, email, password, role)
       VALUES ($1, $2, $3, $4)
     `, [username, email, hashedPassword, role])
 
     res.status(201).send('Akun berhasil disisipkan');
-
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
     res.status(500).json({ error: 'Gagal menyisipkan data akun' });
   }
-})
+});
 
 app.post('/login', async (req, res) => {
-  const users = await db.query(`SELECT * FROM users`)
+  const users = await db.query(`SELECT * FROM public.users`)
   const user = users.find(user => user.username === req.body.username)
   if (user == null) {
     return res.status(400).send('Tidak dapat menemukan user')
   } 
   try {
     if (await bcrypt.compare(req.body.password, user.password)) {
-      // res.send('Login berhasil')
-
       const accessToken = generateAccessToken(user)
       const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
       await db.query(`
@@ -96,10 +168,10 @@ app.post('/login', async (req, res) => {
       res.status(401).send('Username atau password salah')
     }
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
     res.status(500).json({ error: 'Gagal login' });
   }
-})
+});
 
 app.delete('/logout', async (req, res) => {
   try {
@@ -108,12 +180,12 @@ app.delete('/logout', async (req, res) => {
     `, [req.body.refreshToken])
     res.status(204).send('Berhasil logout')
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
     res.status(500).json({ error: 'Gagal logout' });
   }
-})
+});
 
 // Start server
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`)
-})
+});
