016f6f6d13a4709f6a924ff1dc404c7bb05fc325
Aprilyanto Siburian <yantosetiyawan550@gmail.com>
2023-11-21 17:14:28 +0700

    jnjnj

--- server/app/controllers/user.controller.js
+++ server/app/controllers/user.controller.js
@@ -40,4 +40,29 @@ exports.signup = async (req, res) => {
   }
 }
 
-exports.generateMahasiswa
+exports.generate = async (req, res) => {
+  try {
+    if (req.body.nim) {
+      const nim = req.body.nim;
+      const user = await User.findOne({ where: { username: nim } });
+      if (user) {
+        res.status(500).send({ message: 'NIM sudah terdaftar' });
+      } else {
+        const salt = await bcrypt.genSalt();
+        const password = await bcrypt.hash(nim, salt);
+        const role = await Role.findOne({ where: { name: 'mahasiswa' } });
+        const mahasiswa = await Mahasiswa.findOne({ where: { nim } });
+        const user = await User.create({
+          username: nim,
+          password,
+          role_id: role.id,
+          mahasiswa_id: mahasiswa.id,
+        });
+        await user.save();
+        res.status(201).send('Akun berhasil disisipkan');
+      }
+    }
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+}
