1ac7ad406a56b28e80e2594845085a76a912f259
nathanndk <Razor091203@gmail.com>
2023-11-23 13:53:31 +0700

    glk

--- server/app.js
+++ server/app.js
@@ -1,32 +1,32 @@
 // Initialization
 require("dotenv").config();
 
-const express = require('express')
-const app = express()
-const { db, initializeData } = require('./app/models')
-const multerUpload = require('./app/services/multer.service');
+const express = require("express");
+const app = express();
+const { db, initializeData } = require("./app/models");
+const multerUpload = require("./app/services/multer.service");
 
 // Using services
-app.use(multerUpload)
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(multerUpload);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Database synchronization
-// db.sequelize.sync({ alter: true, force: false }).then(() => {
-//   console.log('Drop and re-sync db.');
-// });
+db.sequelize.sync({ alter: true, force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
 
 // Database initialization
 initializeData();
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('Bang udah bang')
-})
-app.use('/auth', require('./app/routes/auth.routes'));
-app.use('/user', require('./app/routes/user.routes'));
+app.get("/", (req, res) => {
+  res.send("Bang udah bang");
+});
+app.use("/auth", require("./app/routes/auth.routes"));
+app.use("/user", require("./app/routes/user.routes"));
 
 // Start server
-app.listen((process.env.SERVER_PORT || 8080), () => {
-  console.log(`Server started on http://localhost:${process.env.SERVER_PORT}`)
+app.listen(process.env.SERVER_PORT || 8080, () => {
+  console.log(`Server started on http://localhost:${process.env.SERVER_PORT}`);
 });
