3c59868925ab5e28ad8df10363a51a1aa488b180
Aprilyanto Siburian <yantosetiyawan550@gmail.com>
2023-11-30 00:59:52 +0700

    ds

--- server/app/middlewares/index (1).js
+++
@@ -1,7 +0,0 @@
-const authMiddleware = require("../middlewares/auth.middleware");
-const mahasiswaMiddleware = require("../middlewares/mahasiswa.middleware");
-
-module.exports = {
-    authMiddleware,
-    mahasiswaMiddleware
-}
