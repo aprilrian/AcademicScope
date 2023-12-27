31c9f8ea563ad6ee18126f382ea0cfb1d8139fff
Aprilyanto Siburian <yantosetiyawan550@gmail.com>
2023-12-27 21:38:21 +0700

    Delete server directory

--- server/app/configs/auth.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    SECRET: process.env.SECRET,
-    SECRET_EXP: process.env.SECRET_EXPIRATION,
-    ACC_SECRET: process.env.ACCESS_TOKEN_SECRET,
-    ACC_EXP: process.env.ACCESS_TOKEN_EXPIRATION,
-    REF_SECRET: process.env.REFRESH_TOKEN_SECRET,
-    REF_EXP: process.env.REFRESH_TOKEN_EXPIRATION,
-    SES_SECRET: process.env.SESSION_SECRET,
-};
--- server/app/configs/cors.config.js
+++
@@ -1,9 +0,0 @@
-const cors = require("cors");
-
-const corsOptions = {
-    origin: ["http://localhost:3000", "http://localhost:8080"],
-};
-
-const corsConfig = cors(corsOptions);
-
-module.exports = corsConfig;
