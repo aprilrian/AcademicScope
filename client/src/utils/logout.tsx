5c9373b4de441745664f064b808e3f113ddd1ddd
Aprilyanto Siburian <yantosetiyawan550@gmail.com>
2023-12-27 21:59:44 +0700

    Update

--- client/src/utils/logout.tsx
+++
@@ -1,9 +0,0 @@
-import { deleteCookie } from "cookies-next";
-
-export default function logout() {
-  // Remove accessToken from cookie
-  deleteCookie("accessToken");
-
-  // Redirect to login page
-  window.location.href = "/";
-}
